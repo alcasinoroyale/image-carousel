60ab2111c9dfa1614e579b17e5884bc1685b5367
alcasinoroyale <alexzdatny@gmail.com>
2024-02-12 01:36:47 -0500

    Add Carousel Files

---
+++ src/Carousel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CarouselItem } from "./CarouselItem";
+
+export const Carousel = () => {
+    return  (
+        <div>
+            <CarouselItem />
+            </div>
+    );
+};
